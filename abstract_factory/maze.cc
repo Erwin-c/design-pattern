cef3b4766335ba49cf26c0c9ef510fefbcd0201d
Erwin <274868686@qq.com>
2021-01-18 22:35:10 +0800

    ...

---
+++ abstract_factory/maze.cc
@@ -0,0 +1,33 @@
+/*
+ * maze.cc
+ *
+ *  Created on: Jan 18, 2021
+ *      Author: Erwin
+ */
+
+#include "maze.h"
+
+#include <iostream>
+#include <vector>
+
+Maze::Maze() : m_room_vec_(new std::vector<Room*>) {
+  std::cout << "Maze::Maze()." << std::endl;
+}
+
+Maze::~Maze() { std::cout << "Maze::~Maze()." << std::endl; }
+
+Maze* Maze::Clone() const { return new Maze(*this); }
+
+Room* Maze::RoomNo(int n) const {
+  std::vector<Room*>::const_iterator it = m_room_vec_->begin();
+  for (; it != m_room_vec_->end(); it++) {
+    if ((*it)->room_number() == n) {
+      return (*it);
+      break;
+    }
+  }
+
+  return 0;
+}
+
+void Maze::AddRoom(Room* room) { m_room_vec_->push_back(room); }
